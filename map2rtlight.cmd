d1fb89e2511bea5e53620a5d4b7cc3bb7b9a208e
hansschmucker <hansschmucker@gmail.com>
2015-11-26 10:37:47 +0100

    Initial import for map2rtlights

---
+++ map2rtlight.cmd
@@ -0,0 +1,1 @@
+js.exe map2rtlight.js "%~1.map" > "%~1.rtlights"
---
+++ map2rtlight.js
@@ -0,0 +1,52 @@
+var map=snarf(arguments[0]);
+
+function TextNode(text){
+	this.value=text;
+}
+
+TextNode.prototype.value="";
+
+function WavyNode(){
+	this.properties={};
+}
+
+WavyNode.prototype.childNodes=null;
+WavyNode.prototype.properties=null;
+
+var lights=[];
+
+function parseToTree(src){
+	var root=[];
+	var find=/(?:([^{]+)|\{([^}]*)\})/g;
+	var findProp=/(?:^|\r\n)\s*"([^"]*)"\s+"([^"]*)"/g;
+	var m,m2;
+	
+	while(null!=(m=find.exec(src))){
+		if(m[1])
+			root.push(new TextNode(m[1]));
+		else{
+			var c=new WavyNode();
+			c.childNodes=parseToTree(m[2]);
+			
+			for(var i=0;i<c.childNodes.length;i++){
+				if(!(c.childNodes[i] instanceof TextNode))
+					continue;
+				
+				while(null!=(m2=findProp.exec(c.childNodes[i].value))){
+					c.properties[m2[1].toLowerCase().trim()]=m2[2];
+				}
+			}
+			if(c.properties.classname=="light")
+				lights.push(c.properties);
+			root.push(c);
+		}
+	}
+	return root;
+}
+
+parseToTree(map);
+
+
+for(var i=0;i<lights.length;i++){
+	print(lights[i].origin+" "+lights[i].light+" 1.0 1.0 1.0 "+lights[i].style);
+}
